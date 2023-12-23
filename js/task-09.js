3672004e8e5a0aeafbb724817a0434dc8f0e82f2
Natalia Khodorova <icxodnik@gmail.com>
2023-12-23 09:08:49 +0100

    implement 9-th task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const refSpan = document.querySelector(".color");
+
+document.querySelector(".change-color").addEventListener("click", function onClick() {
+  const randomColor = getRandomHexColor();
+  refSpan.innerText = randomColor;
+  document.body.style.backgroundColor = randomColor;
+})
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
